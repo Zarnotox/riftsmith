8ef0f5dd6b6a0b290b6480c413d87e8215a43083
Zarnotox <zarnotox@gmail.com>
2025-12-27 04:19:16 +0000

    initial devcontainer

---
+++ riftbound-data-syncer/src/main/java/com/riftsmith/riftbound_data_syncer/RiftboundDataSyncerApplication.java
@@ -0,0 +1,13 @@
+package com.riftsmith.riftbound_data_syncer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiftboundDataSyncerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiftboundDataSyncerApplication.class, args);
+	}
+
+}
---
+++ riftbound-data-syncer/src/test/java/com/riftsmith/riftbound_data_syncer/RiftboundDataSyncerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riftsmith.riftbound_data_syncer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiftboundDataSyncerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
